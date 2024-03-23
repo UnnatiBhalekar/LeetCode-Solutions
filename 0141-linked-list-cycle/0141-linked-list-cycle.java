d3b6b3481602732c498d670e44770a37b224bd79
Unnati Bhalekar <59570663+UnnatiBhalekar@users.noreply.github.com>
2024-03-23 20:07:59 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (26.49%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,7 +13,7 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        
+        // two pointer approach - slow and fast pointers
         while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
