72cec6ee05c174e91255eedaa6ff8c242a1ae58a
Unnati Bhalekar <59570663+UnnatiBhalekar@users.noreply.github.com>
2026-09-09 11:34:50 +0100

    Time: 5 ms (63.11%), Space: 44 MB (97.65%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+        for (int f : freq) {
+            if (f != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
