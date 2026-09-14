8a9f8e8d03f979325b3f610df7ecd82713df7f4b
Unnati Bhalekar <59570663+UnnatiBhalekar@users.noreply.github.com>
2026-09-14 10:42:20 +0100

    Time: 3 ms (23.58%), Space: 65.2 MB (73.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,23 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
-        int[] output = new int [n];
+        //creating left and right sub-array
+        int[] leftSubArrayProduct = new int[nums.length];
+        int[] rightSubArrayProduct = new int[nums.length];
 
-        prefix[0]=1;
-        for(int i = 1; i < n; i++){
-            prefix[i] = prefix[i-1] * nums[i-1];
+        //calculating the left subarray product
+        leftSubArrayProduct[0] = 1;
+        for (int i = 1; i < leftSubArrayProduct.length; i++) {
+            leftSubArrayProduct[i] = leftSubArrayProduct[i - 1] * nums[i - 1];
         }
 
-        suffix[n - 1] = 1;
-        for(int i = n-2; i >= 0; i --){
-            suffix[i] = suffix[i + 1] * nums[i + 1];
+        //calculating the right subarrya product
+        rightSubArrayProduct[rightSubArrayProduct.length - 1] = 1;
+        for (int i = rightSubArrayProduct.length - 2; i > -1; i--) {
+            rightSubArrayProduct[i] = rightSubArrayProduct[i + 1] * nums[i + 1];
         }
 
-        for(int i = 0; i < n; i ++){
-            output[i] = prefix[i] * suffix[i];
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = leftSubArrayProduct[i] * rightSubArrayProduct[i];
         }
-        return output;
+
+        return ans;
     }
 }
